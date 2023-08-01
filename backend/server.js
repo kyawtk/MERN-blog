c17495cd93dc93caed6ebca5717f8a8cf3849bb5
kyawtk <kyawthantkhaing@gmail.com>
2023-08-01 18:33:24 +0630

    changed cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ import asyncHandler from "express-async-handler";
 connectDB();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173",credentials: true}));
+app.use(cors({ origin: "https://blog-backend-hibu.onrender.com",credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
